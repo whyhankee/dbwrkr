4bb8729520620521ae52732fd1e4abd2c29c09b5
Paul Wijhenke <paul@paulsplace.nl>
2017-06-23 11:57:34 +0200

    Add middleware ‘once’

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 const DBWrkr = require('./lib/dbwrkr');
+const middleware = require('./lib/middleware');
 const tests = require('./lib/dbwrkr-tests');
 
 
 module.exports = {
   DBWrkr: DBWrkr,
+  middleware: middleware,
+
   tests: tests,
 };
-
