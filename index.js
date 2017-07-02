c41ac08cbd24807ed3866f2064d9c3b5e5d03140
Paul Wijhenke <paul@paulsplace.nl>
2017-07-02 13:23:43 +0200

    Rename test file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const DBWrkr = require('./lib/dbwrkr');
 const middleware = require('./lib/middleware');
-const tests = require('./lib/dbwrkr-tests');
+const tests = require('./lib/tests');
 
 
 module.exports = {
